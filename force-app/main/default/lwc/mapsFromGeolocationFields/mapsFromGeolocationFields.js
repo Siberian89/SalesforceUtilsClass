7ada722ab0410582a76ffd024273911ed7674c9e
“alessandro.damito” <damito.a@gmail.com>
2023-10-18 17:12:35 +0200

    better JS for MapsFromGeolocationFields LWC

--- force-app/main/default/lwc/mapsFromGeolocationFields/mapsFromGeolocationFields.js
+++ force-app/main/default/lwc/mapsFromGeolocationFields/mapsFromGeolocationFields.js
@@ -6,52 +6,58 @@
  * @last modified on  : 2023-10-18
  * @last modified by  : Siberian89
 **/
-import { LightningElement, api, wire } from 'lwc';
+import { LightningElement, api, wire, track } from 'lwc';
 import { getRecord } from 'lightning/uiRecordApi';
 
 const ERROR_MESSAGE_PARAGRAPH_CLASS = 'slds-align_absolute-center errorMessageTextColor';
 
-//fields that cannot be retrieved from layout
-const DEFAULT_FIELDS = [];
-
 export default class MapsFromGeolocationFields extends LightningElement {
+    //autopopulated fields
     @api recordId;
+    @api objectApiName;
+    //fields populated throu builder 
     @api cmpTitle;
     @api errorMessage;
     @api longitudeFieldApiName;
     @api latitudeFieldApiName;
-
+    //runtime generated fields
+    @track fields;
+    //initializing variable
     errorMessageClass = ERROR_MESSAGE_PARAGRAPH_CLASS;
-
     mapMarkers = [];
 
-    @wire(getRecord, {recordId: '$recordId', layoutTypes: ['Full'], modes: ['View'], optionalFields: DEFAULT_FIELDS})
+    @wire(getRecord, {recordId: "$recordId", fields: "$fields" })
     wiredRecord({ error, data }) {
         if (data) {
-
-            if(!this.longitudeFieldApiName || !this.latitudeFieldApiName){
-                this.mapMarkers = [];
-                console.error('error in fieldApiName definition');
+            if(!this.longitudeFieldApiName || !this.latitudeFieldApiName || !data?.fields[this.longitudeFieldApiName]?.value || !data?.fields[this.latitudeFieldApiName]?.value){
+                this.handleError('error in fieldApiName definition');
                 return;
             }
 
-            const LONGITUDE = data.fields[this.longitudeFieldApiName].value;
-            const LATITUDE = data.fields[this.latitudeFieldApiName].value;
+            const LONGITUDE = data?.fields[this.longitudeFieldApiName]?.value;
+            const LATITUDE = data?.fields[this.latitudeFieldApiName]?.value;
 
             if(!LONGITUDE || !LATITUDE){
-                this.mapMarkers = [];
+                this.handleError('error getting longitude or latitude value');
             }else{
                 this.updateMap(LONGITUDE, LATITUDE);
             }
-
         } else if (error) {
-            this.mapMarkers = [];
-            console.error(error);
+            this.handleError(error);
         }
+    }
+
+    connectedCallback() {
+        this.fields = [this.objectApiName + '.' + this.longitudeFieldApiName, this.objectApiName + '.' + this.latitudeFieldApiName];
     }
 
     updateMap(Longitude, Latitude) {
         this.mapMarkers = [{location: { Latitude, Longitude }}];
+    }
+
+    handleError(error){
+        this.mapMarkers = [];
+        console.error(error);
     }
 
     get showMap() {
